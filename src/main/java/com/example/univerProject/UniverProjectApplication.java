eb69f85b97af65bd2686561ad13c160b1cfaa7c5
Feona <rerrosr@gmail.com>
2024-02-29 15:00:59 +0300

    new

---
+++ src/main/java/com/example/univerProject/UniverProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.univerProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniverProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniverProjectApplication.class, args);
+	}
+
+}
